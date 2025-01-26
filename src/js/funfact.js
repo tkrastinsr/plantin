22438a1af70259c65a8ea2896fa51dcca22cd907
tomskrastins-rullis <toms.krastins-rullis@student.howest.be>
2025-01-26 02:22:12 +0100

    added fun fact function

---
+++ src/js/funfact.js
@@ -0,0 +1,27 @@
+const $funButtons = document.querySelectorAll(".fun-fact__button");
+const $closeButtons = document.querySelectorAll(".close__button");
+
+const handleOpenFact = (e) => {
+  const button = e.currentTarget;
+  const factId = button.getAttribute("data-dialog");
+  const fact = document.getElementById(factId);
+  if (fact) {
+    fact.showModal();
+  }
+};
+
+const handleCloseFact = (e) => {
+  const closeFact = e.target.closest("dialog");
+  closeFact.close();
+};
+
+const init = () => {
+  $funButtons.forEach((button) => {
+    button.addEventListener("click", handleOpenFact);
+  });
+  $closeButtons.forEach((close) => {
+    close.addEventListener("click", handleCloseFact);
+  });
+};
+
+init();
